d5355baa38e882f23b3b4cb39b201eaa13e43dbb
Asia Carter <asiamcarter@gmail.com>
2018-11-29 13:32:11 -0600

    getting there...

---
+++ audrey.js
@@ -0,0 +1,35 @@
+const audrey = document.getElementById("audrey")
+
+
+// /*
+//     Add an event listener to the `document` object to listen
+//     for the "scroll" event.
+// */
+window.addEventListener('scroll', function()  {
+    console.log("hi there!")
+    audrey.style.transform = "scale(8,8)"
+    
+    
+
+});
+
+
+
+//         Adjust the width of audrey to be 1/3 the value of
+//         `window.scrollY`. No lower than 50px, though.
+//     */
+
+
+
+//     /*
+//         Adjust the height of audrey to be 1/4 the value of
+//         `window.scrollY`. No lower than 100px, though.
+//     */
+// })
+
+
+
+
+
+
+
